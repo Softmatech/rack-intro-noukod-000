d4ace60c99d9625c223afda4add13db31b83ed80
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-14 18:01:53 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World my name is Andy Feidje Joseph"
+    resp.write "Hello, my name is"
     resp.finish
   end
 
